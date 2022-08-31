10f5cf19fe20a501c517b59ca09695131ec77a67
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2022-08-31 13:44:06 -0400

    touching up navbar css

--- frontline-church/src/components/Body.js
+++ frontline-church/src/components/Body.js
@@ -6,11 +6,20 @@ import { Button } from "./Button";
 
 function Body() {
  return (
-  <div className='body-container'>
-   <h1>FRONTLINE CHURCH</h1>
-   <p>"We loved him because he loved us first"</p>
-   <div className='body-btns'>
-    <Button className='btn-large'>Visit Us</Button>
+  <div className='main-container'>
+   <div className='home-container'>
+    <h1>FRONTLINE CHURCH</h1>
+    <p>"We loved him, because he first loved us"</p>
+    <p className='verse'>1 John 4:19</p>
+    <div className='home-btns'>
+     <Button className='btn-large'>Visit Us</Button>
+    </div>
+   </div>
+   <div>
+    <div className='about-container'>
+     <h1>About Us</h1>
+     <p>FRONTLINE Church was established in 2015 with a small gathering of 10 families.</p>
+    </div>
    </div>
   </div>
  );
